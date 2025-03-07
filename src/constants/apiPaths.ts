565635f88746d2a9672dddca3d5547a1b79ddbea
a.kashina <a.kashina@websecret.by>
2025-03-07 11:03:28 +0100

    fix(api): update import API path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://mumn7d9j79.execute-api.eu-central-1.amazonaws.com/prod",
   bff: "https://oj77l1z93b.execute-api.eu-central-1.amazonaws.com/prod",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
