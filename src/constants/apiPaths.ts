ba4a853c7aee363a5718040b7ec0c5e081c42d19
a.kashina <a.kashina@websecret.by>
2025-04-02 13:12:50 +0200

    fix(api): integrate cart service

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,10 @@
 const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  order:
+    "https://qamr6xxqni.execute-api.eu-central-1.amazonaws.com/prod/api/profile/cart",
   import: "https://mumn7d9j79.execute-api.eu-central-1.amazonaws.com/prod",
   bff: "https://oj77l1z93b.execute-api.eu-central-1.amazonaws.com/prod",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "https://qamr6xxqni.execute-api.eu-central-1.amazonaws.com/prod/api",
 };
 
 export default API_PATHS;
