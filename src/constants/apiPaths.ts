1a83bd5954dddd59690d93eafeb0a79e96dc5b06
a.kashina <a.kashina@websecret.by>
2025-04-13 12:52:29 +0200

    fix(api): update order and cart API paths

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,10 +1,9 @@
 const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  order:
-    "https://qamr6xxqni.execute-api.eu-central-1.amazonaws.com/prod/api/profile/cart",
+  order: "https://d2qja5et1svlhc.cloudfront.net/api/profile/cart",
   import: "https://mumn7d9j79.execute-api.eu-central-1.amazonaws.com/prod",
   bff: "https://oj77l1z93b.execute-api.eu-central-1.amazonaws.com/prod",
-  cart: "https://qamr6xxqni.execute-api.eu-central-1.amazonaws.com/prod/api",
+  cart: "https://d2qja5et1svlhc.cloudfront.net/api",
 };
 
 export default API_PATHS;
