1a732e338ab37af541604773dafc6dfb98f8e6a5
a.kashina <a.kashina@websecret.by>
2025-04-19 17:40:15 +0200

    fix(api): update product API paths

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  order: "https://d2qja5et1svlhc.cloudfront.net/api/profile/cart",
+  order: "https://d156fpdq5aqlf7.cloudfront.net/cart/api/profile/cart",
   import: "https://mumn7d9j79.execute-api.eu-central-1.amazonaws.com/prod",
-  bff: "https://oj77l1z93b.execute-api.eu-central-1.amazonaws.com/prod",
-  cart: "https://d2qja5et1svlhc.cloudfront.net/api",
+  bff: "https://d156fpdq5aqlf7.cloudfront.net",
+  cart: "https://d156fpdq5aqlf7.cloudfront.net/cart/api",
 };
 
 export default API_PATHS;
